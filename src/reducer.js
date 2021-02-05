34c196e16d4c92ea6186ce44205c24dfe87101e2
murat26 <74151892+muratyayla26@users.noreply.github.com>
2021-02-05 20:23:27 +0300

    CartItem actions are added and connected to store

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,42 @@
-import { INCREASE, DECREASE } from "./actions";
+import { INCREASE, DECREASE, CLEAR_CART, REMOVE } from "./actions";
 
 const reducer = (state, action) => {
-  
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
+  if (action.type === DECREASE) {
+    let cartHolder = [];
+    if (action.payload.amount === 1) {
+      cartHolder = state.cart.filter((cart) => {
+        return cart.id !== action.payload.id;
+      });
+    } else {
+      cartHolder = state.cart.map((cart) => {
+        if (cart.id === action.payload.id) {
+          return { ...cart, amount: cart.amount - 1 };
+        }
+        return cart;
+      });
+    }
+    return { ...state, cart: cartHolder };
+  }
+  if (action.type === INCREASE) {
+    let cartHolder = state.cart.map((cart) => {
+      if (cart.id === action.payload.id) {
+        return { ...cart, amount: cart.amount + 1 };
+      }
+      return cart;
+    });
+    return { ...state, cart: cartHolder };
+  }
+  if (action.type === REMOVE) {
+    return {
+      ...state,
+      cart: state.cart.filter((cart) => {
+        return cart.id !== action.payload.id;
+      }),
+    };
+  }
   return state;
 };
 
