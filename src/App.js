122b4df24d2d75f7590511d797ce7e50f90957e6
murat26 <74151892+muratyayla26@users.noreply.github.com>
2021-02-05 16:44:48 +0300

    actions are declared in a separate file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
-// components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
-import cartItems from "./cart-items";
-// redux stuff
+import cartItems from "./cart-items"; //static items
+import { createStore } from "redux";
+import { DECREASE, INCREASE } from "./actions";
 
+const initialStore = {
+  count: 0,
+  name: "murat",
+};
+
+const reducer = (state, action) => {
+  if (action.type === DECREASE) {
+    return { ...state, count: state.count - 1 };
+  }
+  if (action.type === INCREASE) {
+    return { ...state, count: state.count + 1, name: "yayla" };
+  }
+  return state;
+};
+
+const store = createStore(reducer, initialStore);
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE });
+
+console.log(store.getState());
 function App() {
-  // cart setup
-
   return (
     <main>
       <Navbar />
